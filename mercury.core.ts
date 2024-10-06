acc20d884079ec006cce06f613ff35a74fcfd817
dagre45 <109938306+dagre45@users.noreply.github.com>
2024-10-06 09:10:14 -0700

    Update mercury.core.ts

--- mercury.core.ts
+++ mercury.core.ts
@@ -1,13 +1,14 @@
+
 // Configuration file for Mercury Core. Hover over a property to see its description!
 // If you're running in production, you'll need to rebuild Mercury Core to apply changes.
 
 export default {
 	Name: "Neptune",
-	Domain: "neptunegaming.xyz",
+	Domain: "betterblox.win",
 	DatabaseURL: "http://localhost:8000",
 	RCCServiceProxyURL: "http://localhost:64990",
 	LauncherURI: "mercury-player:",
-	CurrencySymbol: "屌",
+	CurrencySymbol: "又",
 	Pages: ["Statistics", "Forum"],
 
 	// 'noob' colours
@@ -29,14 +30,14 @@ export default {
 	Branding: {
 		Favicon: "Branding/Favicon.svg",
 		Icon: "Branding/Icon.svg",
-		Tagline: "Revival tagline",
+		Tagline: "Dive into the fun, surface with friends",
 		Descriptions: {
 			"Endless possibilites":
 				"Create or play your favourite games and customise your character with items on our catalog.",
-			"New features":
-				"In addition to full client usability, additional features such as security fixes, QoL fixes and an easy to use website make your experience better.",
 			"Same nostalgia":
-				"All of our clients will remain as vanilla as possible, to make sure it's exactly as you remember it.",
+				"All of our clients will remain as vanilla as possible, to make sure it's exactly as you remember it..",
+			"Everything is connected":
+				"Connect with your friends via forums, games, And sharing your thoughts via the status feature.",
 		},
 	},
 
@@ -53,6 +54,10 @@ export default {
 		{
 			Name: "Standard",
 			Path: "Themes/Standard.css",
+                        },
+                  {
+                  Name: "Blue",
+			Path: "Themes/Standard.css",        
 		},
 	],
 
@@ -64,6 +69,6 @@ export default {
 
 	RegistrationKeys: {
 		Enabled: true,
-		Prefix: "mercurkey-",
+		Prefix: "neptunekey-",
 	},
 } satisfies import("./Assets/schema.ts").Config
